6e2223c1acaa56df35909e6159ad6f0c57a14ff4
Tom_Court <tomcourt94@icloud.com>
2023-09-08 15:29:37 +0100

    homepage and enter page

--- src/App.js
+++ src/App.js
@@ -20,21 +20,13 @@ function App() {
             transition={{ duration: 1.5 }}
             className="landing-page"
           >
-            <motion.h1
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.5 }}
-            >
-              Tom Court: Software Engineer
-            </motion.h1>
             <div className="center-button">
               <motion.button
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ delay: 1 }}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
                 onClick={handleButtonClick}
+                className="cool-button"
               >
                 Enter
               </motion.button>
@@ -46,10 +38,34 @@ function App() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <h1>Tom Court: Software Engineer</h1>
-            <h2>About Me</h2>
-            <h2>My Projects</h2>
-            <h2>Contact Me</h2>
+            <motion.h1
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              Tom Court: Software Engineer
+            </motion.h1>
+            <motion.h2
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.2 }}
+            >
+              About Me
+            </motion.h2>
+            <motion.h2
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.6 }}
+            >
+              My Projects
+            </motion.h2>
+            <motion.h2
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 0.8 }}
+            >
+              Contact Me
+            </motion.h2>
           </motion.div>
         )}
       </AnimatePresence>
