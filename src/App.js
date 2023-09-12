9042b6af12b25a194be0b756088b3ba9224af4d5
Tom_Court <tomcourt94@icloud.com>
2023-09-12 16:43:37 +0100

    failsafe

--- src/App.js
+++ src/App.js
@@ -14,42 +14,59 @@ function scrollTo(target) {
 }
 
 function App() {
- 
-return (
-  <div className="container">
-    <div className="home-container">
-      < Home />
-      <h1 class="home-page-header">Tom Court: Software Engineer</h1>
-      <img className="picture" src="https://i.imgur.com/NF2gNgt.png"></img>
-      <div className="container-links">
-        <a onClick={() => scrollTo("#skills")}>Skills and Experience</a>
-        <br />
-        <a onClick={() => scrollTo("#projects")}>Projects</a>
-        <br />
-        <a onClick={() => scrollTo("#about")}>About</a>
-        <br />
-        <a onClick={() => scrollTo("#contact")}>Contact</a>
-      </div>
-    </div>
+  const [showContent, setShowContent] = useState(false);
 
-    <div className="skills-container" id="skills">
-      < Skills />
-    </div>
+  const handleEnterClick = () => {
+    setShowContent(true);
+  };
 
-    <div className="projects-container" id="projects">
-      < Projects />
-      
+  return (
+    <div className="container">
+      {!showContent ? (
+        <div className="landing-page">
+          <button onClick={handleEnterClick}>Enter</button>
+        </div>
+      ) : (
+        <div className="home-container">
+          <Home />
+          <h1 className="home-page-header">Tom Court: Software Engineer</h1>
+          <img
+            className="picture"
+            src="https://i.imgur.com/NF2gNgt.png"
+            alt="Tom Court"
+          ></img>
+          <div className="container-links">
+            <a onClick={() => scrollTo("#skills")}>Skills and Experience</a>
+            <br />
+            <a onClick={() => scrollTo("#projects")}>Projects</a>
+            <br />
+            <a onClick={() => scrollTo("#about")}>About</a>
+            <br />
+            <a onClick={() => scrollTo("#contact")}>Contact</a>
+          </div>
+        </div>
+      )}
+
+      {showContent && (
+        <div>
+          <div className="skills-container" id="skills">
+            <Skills />
+          </div>
+
+          <div className="projects-container" id="projects">
+            <Projects />
+          </div>
+          <div className="about-container" id="about">
+            <About />
+          </div>
+
+          <div className="contact-container" id="contact">
+            <Contact />
+          </div>
+        </div>
+      )}
     </div>
-    <div className="about-container" id="about">
-      < About />
-    </div>
-    
-    <div className="contact-container" id="contact">
-      < Contact />
-    </div>
-    
-  </div>
-);
+  );
 }
 
 export default App;
